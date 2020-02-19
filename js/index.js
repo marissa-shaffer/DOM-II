099d407a9a1bb715024c4aede0f07ce433122342
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-02-18 19:01:25 -0600

    added mouseover event to nav anchors

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Your code goes here
 
 // mouseover
+const anchors = Array.from(document.querySelectorAll('.nav a'));
+anchors.forEach(e => e.addEventListener('mouseover',(event) => {
+    event.currentTarget.style.color = '#AC33FF';
+}));
 
 // keydown
 
